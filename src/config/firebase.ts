2830eaa18a8fe65c1e770072c43ad298e7af2e1e
A B M Nasim <abmnasim@outlook.com>
2026-08-17 02:03:34 +0600

    credential issue resolve

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,12 +1,13 @@
-import { cert, initializeApp} from "firebase-admin/app"
+import { cert, initializeApp } from "firebase-admin/app";
 import { getMessaging } from "firebase-admin/messaging";
-import serviceAccount from "../../credentials/firebase-service-account.json" with {
-    type: "json"
-}
-
+import serviceAccount from "../../credentials/firebase-service-account.json" with { type: "json" };
 
 const app = initializeApp({
-    credential: cert(serviceAccount)
-})
+  credential: cert({
+    projectId: serviceAccount.project_id,
+    clientEmail: serviceAccount.client_email,
+    privateKey: serviceAccount.private_key,
+  }),
+});
 
-export const messaging = getMessaging(app)
+export const messaging = getMessaging(app);
